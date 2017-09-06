2ffc7e145b6a74175bbee7451453cb0e85ecf6c1
vyong <vyong@udel.edu>
2017-09-05 22:47:44 -0400

    More unit tests

--- Vectors.Test/VectorsTests.cs
+++ Vectors.Test/VectorsTests.cs
@@ -10,28 +10,37 @@ using Vectors;
 namespace Vectors.Test
 {
     [TestFixture]
-    public class VectorsTests
+    public class Cross3DTests
     {
         Vector a = new Vector(1, 2, 3);
         Vector b = new Vector(4, 5, 6);
         Vector c = new Vector(7, 8, 9);
+        Vector d = new Vector(1, 7, 9);
+        Vector e = new Vector(8, 2, 4);
 
         [Test]
         public void Cross3DTest1()
         {
             Assert.That(VectorOperation.Cross3D(a, b).Equals(new Vector(-3, 6, -3)));
+            Assert.That(!VectorOperation.Cross3D(a, b).Equals(VectorOperation.Cross3D(b, a)));
         }
 
         [Test]
         public void Cross3DTest2()
         {
-            Assert.That(VectorOperation.Cross3D(c, b).Equals(new Vector(-3, 6, -3)));
+            Assert.That(VectorOperation.Cross3D(c, b).Equals(new Vector(3, -6, 3)));
         }
 
         [Test]
         public void Cross3DTest3()
         {
             Assert.That(VectorOperation.Cross3D(b, c).Equals(new Vector(-3, 6, -3)));
+        }
+
+        [Test]
+        public void Cross3DTest4()
+        {
+            Assert.That(VectorOperation.Cross3D(d, e).Equals(new Vector(10, 68, -54)));
         }
     }
 }
